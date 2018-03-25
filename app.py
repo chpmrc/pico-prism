e466b8460496fdb57c23bab4d038db3774523714
Marco Chiappetta <chpmrc@gmail.com>
2018-03-25 00:15:32 +0000

    Fix gravity and cropping

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ app = Flask(__name__)
 def main():
     url = request.args.get('url')
     op = request.args.get('op')
+    width = int(request.args.get('width', 0))
+    height = int(request.args.get('height', 0))
+    crop = bool(request.args.get('crop', False))
+    gravity = request.args.get('gravity', 'bottom_left')
     if op == 'resize':
-        width = int(request.args.get('width'))
-        height = int(request.args.get('height'))
-        return send_file(img2file(resize(url, width, height)), mimetype='image/jpeg')
+        return send_file(img2file(resize(url, width, height, crop=crop, gravity=gravity)), mimetype='image/jpeg')
     else:
         return send_file(img2file(url2img(url)), mimetype='image/jpeg')
-    
--- helpers.py
+++ helpers.py
@@ -3,6 +3,25 @@ from io import BytesIO
 from PIL import Image
 
 
+def get_crop_coordinates(gravity, img, width, height):
+    # Ref: https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#PIL.Image.Image.crop
+    if gravity == 'center':
+        return (
+            img.width / 2 - width / 2,
+            img.height / 2 - height / 2,
+            img.width / 2 + width / 2,
+            img.height / 2 + height / 2,
+        )
+    if gravity == 'top_left':
+        return (0, 0, width, height)
+    if gravity == 'top_right':
+        return (img.width - width, 0, img.width, height)
+    if gravity == 'bottom_right':
+        return (img.width - width, img.height - height, img.width, img.height)
+    if gravity == 'bottom_left':
+        return (0, img.height - height, width, img.height)
+
+
 def url2img(url):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
@@ -17,6 +36,29 @@ def img2file(img):
     return img_io
 
 
-def resize(url, width, height):
+def resize(url, width=0, height=0, crop=False, gravity='top_left'):
     img = url2img(url)
-    return img.resize((width, height))
+    new_height = height
+    new_width = width
+    only_width = bool(width and not height)
+    only_height = bool(height and not width)
+    neither = bool(not width and not height)
+    is_portrait = img.height >= img.width
+    is_landscape = not is_portrait
+    if only_width or (crop and is_landscape):
+        wpercent = (width/float(img.size[0]))
+        height = int((float(img.size[1])*float(wpercent)))
+    if only_height or (crop and is_portrait):
+        hpercent = (height/float(img.size[1]))
+        width = int((float(img.size[0])*float(hpercent)))
+    if neither:
+        width = img.width
+        height = img.height
+    img = img.resize((width, height), Image.ANTIALIAS)
+    if crop:
+        if is_portrait:
+            coordinates = get_crop_coordinates(gravity, img, new_width, img.height)
+        else:
+            coordinates = get_crop_coordinates(gravity, img, img.width, new_height)
+        img = img.crop(coordinates)
+    return img
